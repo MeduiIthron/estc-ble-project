c665a2bddfd7b5a6e68502a181300ee6e84dde65
MeduiIthron <meduiithron@gmail.com>
2022-05-28 07:50:29 +0300

    Add characteristic to the custom service
    Closes #1

--- estc_service.c
+++ estc_service.c
@@ -7,6 +7,8 @@
 #include "ble_gatts.h"
 #include "ble_srv_common.h"
 
+static ret_code_t estc_ble_add_characteristics(ble_estc_service_t *service);
+
 ret_code_t estc_ble_service_init(ble_estc_service_t *service)
 {
     ret_code_t error_code = NRF_SUCCESS;
@@ -16,8 +18,6 @@ ret_code_t estc_ble_service_init(ble_estc_service_t *service)
 
     ble_uuid128_t m_base_uuid128 = {
         ESTC_BASE_UUID
-    
-
     };
     error_code = sd_ble_uuid_vs_add(&m_base_uuid128, &service_uuid.type);
     APP_ERROR_CHECK(error_code);
@@ -28,6 +28,49 @@ ret_code_t estc_ble_service_init(ble_estc_service_t *service)
     NRF_LOG_DEBUG("%s:%d | Service UUID: 0x%04x", __FUNCTION__, __LINE__, service_uuid.uuid);
     NRF_LOG_DEBUG("%s:%d | Service UUID type: 0x%02x", __FUNCTION__, __LINE__, service_uuid.type);
     NRF_LOG_DEBUG("%s:%d | Service handle: 0x%04x", __FUNCTION__, __LINE__, service->service_handle);
+
+    return estc_ble_add_characteristics(service);
+}
+
+static ret_code_t estc_ble_add_characteristics(ble_estc_service_t *service)
+{
+    ret_code_t error_code = NRF_SUCCESS;
+    ble_uuid128_t service_uuid = {
+        ESTC_BASE_UUID
+    };
+
+    ble_uuid_t char_uuid = {
+        .uuid = ESTC_GATT_CHAR_UUID,
+        .type = BLE_UUID_TYPE_VENDOR_BEGIN,
+    };
+
+    error_code = sd_ble_uuid_vs_add(&service_uuid, &char_uuid.type);
+    APP_ERROR_CHECK(error_code);
+
+    ble_gatts_char_md_t char_md = { 0 };
+    char_md.char_props.read = 1;
+    char_md.char_props.write = 1;
+
+    uint8_t description[] = "Test BLE GATT Characteristic";
+    char_md.p_char_user_desc = description;
+    char_md.char_user_desc_size = sizeof(description);
+    char_md.char_user_desc_max_size = sizeof(description);
+
+    ble_gatts_attr_md_t attr_md = { 0 };
+    attr_md.vloc = BLE_GATTS_VLOC_STACK;
+
+    BLE_GAP_CONN_SEC_MODE_SET_OPEN(&attr_md.read_perm);
+    BLE_GAP_CONN_SEC_MODE_SET_OPEN(&attr_md.write_perm);
+
+    ble_gatts_attr_t attr_char_value = { 0 };
+    attr_char_value.p_uuid = &char_uuid;
+    attr_char_value.p_attr_md = &attr_md;
+    attr_char_value.init_len = 1;
+    attr_char_value.max_len = 1;
+    attr_char_value.p_value = description;
+
+    error_code = sd_ble_gatts_characteristic_add(service->service_handle, &char_md, &attr_char_value, &service->characteristic_handle);
+    APP_ERROR_CHECK(error_code);
 
     return NRF_SUCCESS;
 }
--- estc_service.h
+++ estc_service.h
@@ -8,12 +8,19 @@
 
 #define ESTC_BASE_UUID { 0x12, 0xDD, 0x29, 0x01, 0x05, 0x05, /* - */ 0x9B, 0xB0, /* - */ 0xAB, 0x4C, /* - */ 0xA2, 0x8D, /* - */ 0x00, 0x00, 0x0C, 0x74 } // UUID: 740c6551-8da2-4cab-b09b-05050129dd12
 #define ESTC_SERVICE_UUID 0x1984
+#define ESTC_GATT_CHAR_UUID 0x1399
 
 typedef struct
 {
     uint16_t service_handle;
+    uint16_t connection_handle;
+    ble_gatts_char_handles_t characteristic_handle;
 } ble_estc_service_t;
 
 ret_code_t estc_ble_service_init(ble_estc_service_t *service);
+
+void estc_ble_service_on_ble_event(const ble_evt_t *ble_evt, void *ctx);
+
+void estc_update_characteristic_value(ble_estc_service_t *service, int32_t *value);
 
 #endif /* ESTC_SERVICE_H__ */
